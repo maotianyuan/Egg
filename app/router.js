02a6a8488ef51ca7efbf1ef98f4d9c7b304d955a
MTY <tianyuan.mao@zebra-c.com>
2019-07-30 21:06:45 +0800

    热力图

--- app/router.js
+++ app/router.js
@@ -6,15 +6,14 @@
 module.exports = app => {
   const { router, controller } = app;
   router.get('/', controller.home.index);
-  // router.get('/all', controller.all.all.index);
-  router.get('/allspread', controller.allspread.allspread.index);
 
+  // 特约店铺
   router.get('/renderHeatMap', controller.heatMap.store.index.index);
   router.get('/downloadFile', controller.heatMap.store.index.downTemplateFile);
 
+  // 位置信息
   router.get('/renderHeatMapPositionExcel', controller.heatMap.position.index.index); // excel所有城市列
   router.get('/renderHeatMapPositionExcelSelf', controller.heatMap.position.index.indexSelfProvince); // excel本省份列
-  router.get('/renderHeatMapPositionJSON', controller.heatMap.position.index.getJSON);
+  router.get('/renderHeatMapPositionJSON', controller.heatMap.position.index.getJSON); // 值返回json数据
 
-  router.get('/juzhu', controller.juzhu.juzhu.index);
 };
